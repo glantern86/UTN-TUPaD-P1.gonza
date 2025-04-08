5ca9ff25cfd2ef93d3ea542b6624b885121dd6fc
Gonza <gonza.prados@gmail.com>
2025-04-08 18:26:28 -0300

    ejercicios hasta el 5

--- 04 Estructuras Repetitivas/prados_TP-04.py
+++ 04 Estructuras Repetitivas/prados_TP-04.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 '''
 ##1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
 ##(incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
@@ -29,7 +31,7 @@ for i in range(num1+1,num2):
     contador += i
 
 print(contador)
-'''
+
 
 #4) Elabora un programa que permita al usuario ingresar números enteros y los sume en
 #secuencia. El programa debe detenerse y mostrar el total acumulado cuando el usuario ingrese
@@ -43,5 +45,21 @@ while True:
         break
 
 print(acumulador)
+'''
+
+##5) Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el
+##programa debe mostrar cuántos intentos fueron necesarios para acertar el número.
+numero = randrange(0,10)
+contador = 0
+
+while True:
+    num = int(input("Ingrese un numero entre 0 y 9 \n"))
+    contador += 1
+
+    if num == numero:
+        print(f"Adivinaste! El número era el: {numero}. Te tomaron {contador} intentos")
+        break
+    else:
+        num = ("Lo siento no adivinaste.\n")
 
 
